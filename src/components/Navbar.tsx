c87892016a87d6c070bada8daf053e2df35de408
reaperxyndrome <crispy_crab_777@protonmail.com>
2024-04-23 14:00:18 +1000

    navbar first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <nav className="flex justify-between items-center p-3 px-7 mb-10 h-20 text-white bg-[#3b3b3b] rounded-lg shadow-xl shadow-slate-500">
+            <h1>Logo</h1>
+            <section className="flex justify-between gap-5">
+                <h2>About</h2>
+                <h2>Portfolio</h2>
+            </section>
+        </nav>
+    )
+}
