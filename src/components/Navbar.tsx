8fb84516fed53c5509e6fe57b71cbc76c02c2837
reaperxyndrome <crispy_crab_777@protonmail.com>
2024-06-14 21:50:00 +1000

    make cursor pointer when hovering over navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ export default function Navbar() {
         <nav className="flex justify-between items-center p-3 px-7 mb-10 h-20 text-white rounded-lg shadow-xl shadow-slate-500">
             <h1>Logo</h1>
             <section className="flex justify-between gap-10">
-                <h2>About</h2>
-                <h2>Portfolio</h2>
+                <h2 className="cursor-pointer">About</h2>
+                <h2 className="cursor-pointer">Portfolio</h2>
             </section>
         </nav>
     )
